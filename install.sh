8a4b4f384c7693f7a82ed2f375925737352db60d
Timo Dekker <timo@poskassasystemen.nl>
2022-02-14 11:29:08 +0100

    New urls

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 wget -c https://github.com/$(wget -q https://github.com/retaildesk/retaildesk_app/releases -O - | grep "retaildesk_app-*.AppImage" | head -n 1 | cut -d '"' -f 2) -P /opt/retaildesk_app.AppImage
-wget -c https://github.com/retaildesk/retaildesk_app/blob/master/retaildesk.desktop ~/Desktop/retaildesk.desktop
-wegt -c https://github.com/retaildesk/retaildesk_app/blob/master/icons/icon.png /usr/share/pixmaps/retaildesk_app.png
+wget -c https://raw.githubusercontent.com/retaildesk/retaildesk_app/master/retaildesk.desktop ~/Desktop/retaildesk.desktop
+wegt -c https://github.com/retaildesk/retaildesk_app/raw/master/icons/Icon.png /usr/share/pixmaps/retaildesk_app.png
 chmod +x ~/Desktop/retaildesk.desktop
 
 
 
+ 
