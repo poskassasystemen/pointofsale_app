84c57c926ad89502d6b5aed43bad7a279caeeef9
retaildesk <67142861+retaildesk@users.noreply.github.com>
2023-06-15 12:49:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 
 gebruikersnaam="retaildesk"
 repo="retaildesk_app"
-
+sudo apt-get -y purge app_retaildesk
 
 release_tag=$(wget -qO- "https://api.github.com/repos/$gebruikersnaam/$repo/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 
 
 download_url=$(wget -qO- "https://api.github.com/repos/$gebruikersnaam/$repo/releases/tags/$release_tag" | grep "browser_download_url.*AppImage" | cut -d : -f 2,3 | tr -d "\"")
-
+sudo rm -f /opt/retaildesk_app.AppImage
 sudo wget -O /opt/retaildesk_app.AppImage $download_url
 
 sudo chmod +x /opt/retaildesk_app.AppImage
